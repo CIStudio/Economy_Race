31da01abe2362fcac9690aceebb59f54e26f061f
KL0O0CK <thursdaymkl@gmail.com>
2024-11-01 21:54:40 +0300

    Commit number 9

---
+++ economy_race/Scenes/Devices/area_2d.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+
+var in_object_area = false
+var ANIM = "on"
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	if in_object_area == true and Input.is_action_just_pressed("take"):
+		ANIM = "off"
+		print("2")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		in_object_area = true
+		print("1")
+	else:
+		in_object_area = false
+		
+	if ANIM == "off":
+		body.anim_off()
+		
