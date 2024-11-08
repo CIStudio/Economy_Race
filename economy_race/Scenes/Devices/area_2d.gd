ac66c20103a7763a9b87fb4c8a14f98eb5c57ee4
KL0O0CK <thursdaymkl@gmail.com>
2024-11-08 12:16:11 +0300

    Commit number 15

--- economy_race/Scenes/Devices/area_2d.gd
+++ economy_race/Scenes/Devices/area_2d.gd
@@ -1,19 +1,27 @@
 extends Area2D
 
 
-var in_object_area = false
+var in_object_area: bool = false
 var device_turned_off: bool = false
-@onready var ANIM =$"../AnimatedSprite2D"
+var presse_mh: bool = true
+@onready var ANIM = $"../AnimatedSprite2D"
+@onready var PRESSE = $"../PressE"
 
 
 func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
+	if in_object_area == true and presse_mh == true:
+		PRESSE.show()
+	else:
+		PRESSE.hide()
 	if in_object_area == true and Input.is_action_just_pressed("take") and not device_turned_off:
 		Global.devices_off += 1
 		device_turned_off = true
 		ANIM.play("off")
+		presse_mh = false
+		PRESSE.hide()
 		
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
