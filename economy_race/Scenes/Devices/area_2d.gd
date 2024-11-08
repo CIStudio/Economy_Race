b238c295ce06a67749169bd290cba11715e551f6
CIStudio <artamiel2280@gmail.com>
2024-11-08 11:28:03 +0300

    Commit number 14

--- economy_race/Scenes/Devices/area_2d.gd
+++ economy_race/Scenes/Devices/area_2d.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 
 var in_object_area = false
+var device_turned_off: bool = false
 @onready var ANIM =$"../AnimatedSprite2D"
 
 
@@ -9,10 +10,11 @@ func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
-	if in_object_area == true and Input.is_action_just_pressed("take"):
-		ANIM.play("off")
+	if in_object_area == true and Input.is_action_just_pressed("take") and not device_turned_off:
 		Global.devices_off += 1
-
+		device_turned_off = true
+		ANIM.play("off")
+		
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		in_object_area = true
--- economy_race/Scripts/levels/Level_1/meneger.gd
+++ economy_race/Scripts/levels/Level_1/meneger.gd
@@ -7,7 +7,6 @@ var game_paused: bool = false
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		game_paused = !game_paused
-		
 	if game_paused == true:
 		get_tree().paused = true
 		pause_menu.show()
@@ -21,10 +20,12 @@ func _on_resume_pressed() -> void:
 
 
 func _on_quit_pressed() -> void:
+	Global.devices_off = 0
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/menu/level_menu.tscn")
 
 
-func _on_reset_pressed() -> void:
+func _on_restart_pressed() -> void:
+	Global.devices_off = 0
 	game_paused = !game_paused
 	get_tree().reload_current_scene()
